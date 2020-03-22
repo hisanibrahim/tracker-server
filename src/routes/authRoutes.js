f8b2db3d7262ccd969d1c871995efc8dbee7e53b
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-22 19:23:28 +0530

    changed router dir to src

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
   res.send("You made a post request");
 });
 

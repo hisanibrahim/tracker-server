a5a4adcbc5765358cafd483836aca268a5409c76
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-22 14:06:32 +0530

    added mongodb connection, added script to nodemon for automatic restarts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-// import express from "express";
+const mongoUri =
+  "mongodb+srv://admin:6XeW2eNxwdYCVf@tracker-cluster-1sx3f.mongodb.net/test?retryWrites=true&w=majority";
 
 const app = express();
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongodb instance..");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo..", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world!");

e190e958d3d38f991eff0f7540a641d9af2eb10c
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-23 11:27:39 +0530

    salting and hashing password before save to mongo

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -10,6 +11,38 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+// pre('save') execute before saving to mongo
+userSchema.pre("save", function(next) {
+  // used 'function' keyword instead arrow function to make use of 'this' keyword
+
+  // this refers record is editing. eg: user
+  const user = this;
+
+  // if user not modified password field return immediately
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  //if user edited password
+  //generate salt first
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    //generate hash with salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      // replace password with hash
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 mongoose.model("User", userSchema);

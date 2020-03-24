17859f4b6599fe5ff998effd1334d8568b017d81
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-24 15:18:02 +0530

    created new post route for creating tracks

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -10,7 +10,23 @@ router.use(requireAuth);
 
 router.get("/tracks", async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id });
-  res.send(tracks);
+  return res.send(tracks);
+});
+
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: "You must provide name and locations." });
+  }
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    return res.send(track);
+  } catch (err) {
+    return res.status(422).send({ error: err.message });
+  }
 });
 
 module.exports = router;
